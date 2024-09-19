117b1b63a413216c93d33439e5dee0dd9a672947
Branimir Nachev <32915919+branimir-nachev@users.noreply.github.com>
2024-09-19 23:10:51 +0300

    With stories - area

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -8,13 +8,29 @@
 
 import UIKit
 
+
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var storyLabel: UILabel!
+    @IBOutlet weak var choice1Button: UIButton!
+    @IBOutlet weak var choice2Button: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        updateUI(viewStory: currentStory)
     }
 
+    @IBAction func choiceMade(_ sender: UIButton) {
+        let userChoice = sender.currentTitle
+        let nextStory = nextStory(userChoice: userChoice!)
+        updateUI(viewStory: nextStory)
+    }
+    
+    func updateUI(viewStory : Story) {
+        storyLabel.text = viewStory.title
+        choice1Button.setTitle(viewStory.choice1, for: .normal)
+        choice2Button.setTitle(viewStory.choice2, for: .normal)
+    }
 
 }
 
